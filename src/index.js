ce4c0705e3f6c755e2c6ac22a4f4a723882bcb0f
胡城 <hucheng@qutoutiao.net>
2019-06-15 17:51:46 +0800

    feat: add .

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-import './index.css'
-console.log('index')
+import createButton from "./button";
+import colorElement from "./colorElement";
+
+const div = document.createElement("div");
+div.innerText = "Hello World";
+div.style.color = "red";
+
+const button = createButton("HEY FIRST BUTTON YO!");
+
+button.addEventListener("click", e => {
+  colorElement(div, "cyan");
+});
+
+document.body.appendChild(button);
+document.body.appendChild(div);
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
-module.exports = (env) => {
-  return {
-    entry: {
-      main: "./src/main.js",
-      index: "./src/index.js"
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// merge webpack config like Object.assign(a,b)
+const webpackMerge = require("webpack-merge"); 
+
+const modeConfig = env => require(`./build/webpack.${env.mode}.js`)(env);
+console.log('tag', modeConfig)
+module.exports = env => {
+  console.log(env);
+
+  return webpackMerge(
+    {
+      entry: {
+        main: "./src/index.js",
+      },
+      mode: env.mode,
+      module: {
+        rules: [
+          { test: /\.css$/, use: 'css-loader' }
+        ]
+      },
+      devServer: {
+        compress: true
+      },
+      plugins: [new webpack.ProgressPlugin(), new HtmlWebpackPlugin()]
     },
-    mode:env.mode,
-    output: {
-      filename: "[name].js"
-    },
-    module: {
-      rules: [
-        { test: /\.css$/, use: 'css-loader' }
-      ]
-    },
-    devServer: {
-      compress: true
-    }
-  };
+    modeConfig(env)
+  );
 };
